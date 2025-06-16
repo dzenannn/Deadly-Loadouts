6ad8ac829dc167ce61986b75d3aca56c21f358b9
dzenannn <mecinovicdzenan3@gmail.com>
2025-06-16 18:24:42 +0200

    feat: initialize Vue 3 project with Vite, added first component and fetch data functionality
    - Created package.json with dependencies for Vue and Axios
    - Added Vite configuration for building and serving the app
    - Implemented main application entry point in main.js
    - Developed App.vue to include HelloWorld component
    - Created HelloWorld.vue component to fetch and display data from an API
    - Added SVG assets for Vite and Vue logos
    - Styled the application with a basic CSS setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const count = ref(0);
+const state = ref(false);
+const p = ref("");
+
+function fetchData() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => {
+      p.value = res.data.title;
+      state.value = true;
+    })
+    .catch((err) => console.log(err));
+}
+</script>
+
+<template>
+  <div>
+    <p>Hello World</p>
+    <button @click="fetchData">Fetch Todo</button>
+    <p v-if="state">{{ p }}</p>
+  </div>
+</template>
+
+<style scoped></style>
