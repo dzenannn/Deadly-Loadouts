be98d377da5242b7fa79119732cbcd38981f4d58
dzenannn <mecinovicdzenan3@gmail.com>
2025-06-19 17:40:48 +0200

    feat: update ThemeToggle component with SVG icons for theme representation; add Sun component for improved visuals

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,11 +1,35 @@
 <template>
   <button @click="toggleTheme" class="theme-toggle">
-    {{ theme === "light" ? "🌙" : "☀️" }}
+    <span v-if="theme === 'light'">erara</span>
+    <span v-if="theme !== 'light'">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+      >
+        <path
+          fill="#d20000"
+          d="M16 2s0-2 2-2s2 2 2 2v2s0 2-2 2s-2-2-2-2zm18 14s2 0 2 2s-2 2-2 2h-2s-2 0-2-2s2-2 2-2zM4 16s2 0 2 2s-2 2-2 2H2s-2 0-2-2s2-2 2-2zm5.121-8.707s1.414 1.414 0 2.828s-2.828 0-2.828 0L4.878 8.708s-1.414-1.414 0-2.829c1.415-1.414 2.829 0 2.829 0zm21 21s1.414 1.414 0 2.828s-2.828 0-2.828 0l-1.414-1.414s-1.414-1.414 0-2.828s2.828 0 2.828 0zm-.413-18.172s-1.414 1.414-2.828 0s0-2.828 0-2.828l1.414-1.414s1.414-1.414 2.828 0s0 2.828 0 2.828zm-21 21s-1.414 1.414-2.828 0s0-2.828 0-2.828l1.414-1.414s1.414-1.414 2.828 0s0 2.828 0 2.828zM16 32s0-2 2-2s2 2 2 2v2s0 2-2 2s-2-2-2-2z"
+          stroke-width="1.5"
+          stroke="#000"
+        />
+        <circle
+          cx="18"
+          cy="18"
+          r="10"
+          fill="#d20000"
+          stroke-width="1.5"
+          stroke="#000"
+        />
+      </svg>
+    </span>
   </button>
 </template>
 
 <script setup>
 import { useTheme } from "../composables/useTheme";
+import Sun from "./ui/Sun.vue";
 /* eslint-disable */
 const { theme, toggleTheme } = useTheme();
 localStorage.setItem("theme", theme.value);
